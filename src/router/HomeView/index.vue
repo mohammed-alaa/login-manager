9974fa109b38425eff27c8ebd87857760c13dc19
Mohamed Alaa <30788249+mohammed-alaa@users.noreply.github.com>
2023-05-30 02:33:14 +0300

    Added grid views

--- src/router/HomeView/index.vue
+++ src/router/HomeView/index.vue
@@ -1,9 +1,10 @@
 <template>
-    <div class="login-view">
-        <LoginList />
-        <LoginHeader />
-        <LoginBody />
-    </div>
+	<div class="login-view d-grid">
+		<SearchLogins />
+		<LoginHeader />
+		<LoginList />
+		<LoginBody />
+	</div>
 </template>
 
 <script setup>
@@ -12,6 +13,7 @@ import { useStore } from 'vuex'
 import LoginList from '@/components/LoginList'
 import LoginHeader from '@/components/LoginHeader'
 import LoginBody from '@/components/LoginBody'
+import SearchLogins from '@/components/SearchLogins'
 
 const store = useStore()
 onMounted(() => store.dispatch('retrieveLogins'))
@@ -20,4 +22,14 @@ onMounted(() => store.dispatch('retrieveLogins'))
 <style scoped lang="sass">
 .login-view
 	height: calc(100vh - var(--app-header-min-height))
+	grid-template-rows: var(--search-height) auto
+	grid-template-columns: var(--login-list-width) auto
+</style>
+
+<style scoped lang="scss">
+.login-view {
+	grid-template-areas:
+		"search info"
+		"list body";
+}
 </style>
