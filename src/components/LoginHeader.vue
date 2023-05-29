022159da2f71d4a26bae206cb59dcdb737a3891d
Mohamed Alaa <30788249+mohammed-alaa@users.noreply.github.com>
2023-05-30 02:40:15 +0300

    Updated UI style

--- src/components/LoginHeader.vue
+++ src/components/LoginHeader.vue
@@ -1,82 +1,59 @@
 <template>
-    <div class="login-header">
-        <SearchLogins />
-        <div class="login-header-info">
-            <div class="logins-number">
-                <h2 class="m-0">{{ getLoginsLength }} Logins</h2>
-            </div>
-            <div class="actions">
-                <AppButton type="button" id="newLogin" @click="createNewLogin" theme="outline-primary"
-                    class="action-button" size="normal" :disabled="isCreateButtonDisabled" :isActive="isCreating">
-                    <AppIcon icon="plus-lg" />
-                    <span class="ms-2">New Login</span>
-                </AppButton>
-                <!-- <AppButton type="button" @click="openSettings" theme="outline-warning" class="action-button" size="normal">
-                    <AppIcon icon="gear-fill" />
-                </AppButton> -->
-            </div>
-        </div>
-    </div>
+	<div class="login-header d-flex flex-row align-items-center justify-content-between position-relative">
+		<div class="text-white">
+			<h2 class="m-0">{{ getLoginsLength }} Logins</h2>
+		</div>
+		<div class="d-flex flex-row align-items-center gap-2">
+			<AppButton id="newLogin" @click="createNewLogin" theme="outline-primary"
+				size="normal" :disabled="isCreateButtonDisabled" :isActive="isCreating">
+				<AppIcon icon="plus-lg" />
+				<span class="ms-2">New Login (n)</span>
+			</AppButton>
+			<AppButton @click="openSettings" theme="outline-warning" size="normal">
+				<AppIcon icon="gear-fill" />
+				<span class="ms-2">settings</span>
+			</AppButton>
+		</div>
+	</div>
 </template>
 
 <script setup>
 import { computed, onMounted, onUnmounted } from 'vue'
 import { useStore } from 'vuex'
-// import { useRouter } from 'vue-router'
+import { useRouter } from 'vue-router'
 import AppButton from '@/components/AppButton'
 import AppIcon from '@/components/AppIcon'
-import SearchLogins from '@/components/SearchLogins'
 
 const store = useStore()
-// const router = useRouter()
+const router = useRouter()
 let newLoginElement = null
 
 const isCreating = computed(() => store.getters.getMode === 'c')
 const isViewing = computed(() => store.getters.getMode === 'v')
 const isCreateButtonDisabled = computed(() => (!isViewing.value && !isCreating.value))
-const getLoginsLength = computed(() => store.getters.getLoginList?.length || 0)
+const getLoginsLength = computed(() => store.getters.getLoginList?.length ?? 0)
 
 const createNewLogin = () => (isCreating.value ? store.dispatch('setViewMode') : store.dispatch('setCreateMode'))
-// const openSettings = () => router.push({ name: 'settings' })
+const openSettings = () => router.push({ name: 'settings' })
 
 const focusnewLogin = (event) => {
-    if ((event.target.localName !== "input") && (event.key === "n")) {
-        event.preventDefault()
-        newLoginElement.click()
-    }
+	if ((event.target.localName !== "input") && (event.key === "n")) {
+		event.preventDefault()
+		newLoginElement.click()
+	}
 }
 onMounted(() => {
-    newLoginElement = document.getElementById("newLogin")
-    document.addEventListener('keydown', focusnewLogin)
+	newLoginElement = document.getElementById("newLogin")
+	document.addEventListener('keydown', focusnewLogin)
 })
 onUnmounted(() => document.removeEventListener('keydown', focusnewLogin))
 </script>
 
 <style scoped lang="sass">
 .login-header
-    border-bottom: 1px solid var(--main-border-color)
-    background-color: #191919
-    display: flex
-    flex-direction: row
-    align-items: center
-    justify-content: space-between
-    width: 100%
-    z-index: 1
-    position: relative
-    min-height: var(--login-header-min-height)
-
-    .login-header-info
-        width: calc(100vw - 100vw * var(--search-flex-gain))
-        display: flex
-        flex-direction: row
-        align-items: center
-        justify-content: space-between
-        padding-left: var(--secondary-start-offset)
-        padding-right: var(--secondary-start-offset)
-
-        .logins-number
-            color: var(--color-white)
-        .actions
-            .action-button
-                margin-inline: .5rem
+	grid-area: info
+	border-bottom: 1px solid var(--main-border-color)
+	background-color: #191919
+	z-index: 1
+	padding-inline: var(--secondary-start-offset)
 </style>
--- src/components/LoginList.vue
+++ src/components/LoginList.vue
@@ -1,22 +1,26 @@
 <template>
-    <div class="login-list">
-        <transition-group name="swap" :key="4">
-            <ol class="list-unstyled m-0 login-list-listbox" v-if="computedFilteredLogins.length" :key="0">
-                <transition-group name="fade" :key="6">
-                    <LoginItem v-for="(login) in filteredLogins()" :key="login.id" :is-active="isLoginActive(login.id)"
-                        @click="activeLoginChange(login.id)" :login="login" />
-                </transition-group>
-            </ol>
-            <div class="empty-logins-list" v-else :key="1">
-                <div class="header">
-                    No logins found
-                </div>
-                <div class="body">
-                    There are no results matching your search.
-                </div>
-            </div>
-        </transition-group>
-    </div>
+	<div class="login-list">
+		<transition-group name="swap">
+			<template v-if="computedFilteredLogins.length">
+				<ol class="list-unstyled m-0 login-list-listbox">
+					<transition-group name="fade" :key="6">
+						<LoginItem v-for="(login) in filteredLogins()" :key="login.id" :is-active="isLoginActive(login.id)"
+							@click="activeLoginChange(login.id)" :login="login" />
+					</transition-group>
+				</ol>
+			</template>
+			<template v-else>
+				<div class="empty-logins-list h-100 d-flex flex-column align-items-center justify-content-center">
+					<div class="text-white">
+						No logins found
+					</div>
+					<div class="body">
+						There are no results matching your search.
+					</div>
+				</div>
+			</template>
+		</transition-group>
+	</div>
 </template>
 
 <script setup>
@@ -40,23 +44,14 @@ const activeLoginChange = (loginId) => store.dispatch('activeLoginChange', login
 <style lang="sass" scoped>
 $login-list-top-offset: calc(var(--app-header-min-height) + var(--login-header-min-height) + 2px)
 .login-list
-    position: absolute
-    top: #{$login-list-top-offset}
-    left: 0
-    width: calc(100vw * var(--search-flex-gain))
-    height: calc(100vh - #{$login-list-top-offset})
-    background-color: var(--main-background-color)
-    border-right: 1px solid var(--main-border-color)
-    overflow: hidden auto !important
-    .empty-logins-list
-        padding: 2rem
-        height: inherit
-        display: flex
-        flex-direction: column
-        justify-content: center
-        .header
-            color: var(--color-white)
-        .body
-            font-size: .9rem
-            color: var(--color-gray)
+	grid-area: list
+	background-color: var(--main-background-color)
+	border-right: 1px solid var(--main-border-color)
+	overflow: hidden auto !important
+
+	.empty-logins-list
+		padding: 2rem
+		.body
+			font-size: .9rem
+			color: var(--color-gray)
 </style>
