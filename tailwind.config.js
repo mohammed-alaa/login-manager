d852581354b0eb4bca27542f05e3a286b340b730
Mohamed Alaa <30788249+mohammed-alaa@users.noreply.github.com>
2023-11-01 02:24:10 +0200

    Replaced `bootstrap` with `tailwind`

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./src/renderer/**/*.vue", "./src/assets/styles/**/*.sass"],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+}
