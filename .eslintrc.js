68af4337bfbd133cef93e54d5bbb0a6bb1150575
Mohamed Alaa <30788249+mohammed-alaa@users.noreply.github.com>
2023-11-01 02:22:22 +0200

    Added `Eslint`, `prettier` configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,33 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended'
-  ],
-  parserOptions: {
-    parser: '@babel/eslint-parser'
-  },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-extra-boolean-cast': 'off',
-    'vue/multi-word-component-names': 'off',
-  }
+	root: true,
+	env: {
+		node: true,
+		browser: true,
+		commonjs: true,
+	},
+	extends: [
+		"@vue/prettier",
+		"plugin:vue/vue3-essential",
+		"plugin:vue/vue3-recommended",
+		// "@vue/typescript/recommended",
+		"@vue/eslint-config-prettier",
+	],
+	// parserOptions: {
+	// parser: "@typescript-eslint/parser",
+	// },
+	// plugins: ["vue", "@typescript-eslint"],
+	plugins: ["vue"],
+	rules: {
+		"no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+		"no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+		"no-extra-boolean-cast": "off",
+		"vue/multi-word-component-names": "off",
+		"@typescript-eslint/no-explicit-any": "off",
+		"@typescript-eslint/explicit-module-boundary-types": "off",
+		// "@typescript-eslint/no-unused-vars": [
+		// 	"error",
+		// 	{ argsIgnorePattern: "^_" },
+		// ],
+	},
+	ignorePatterns: ["build", "out", "release", "dist"],
 }
